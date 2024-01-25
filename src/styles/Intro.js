199b44e552f182c719938d89f8506d1d8565b0fc
Dan-Koyuki <dankoyuki398@gmail.com>
2024-01-25 17:24:36 +0700

    placeholder

--- src/styles/Intro.js
+++ src/styles/Intro.js
@@ -13,9 +13,9 @@ export const IntroImage = styled.div`
 
   img {
     width: 100%;
-    max-width: 710px;
+    max-width: 500px;
     height: 100%;
-    max-height: 570px;
+    max-height: 500px;
     padding-top: 0.8rem;
     padding-left: 0.9rem;
     padding-right: 0.3rem;
@@ -34,7 +34,7 @@ export const IntroDesc = styled.div`
   margin-left: 1rem;
 
   @media (max-width: 555px) {
-    background: url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/36e1d7e5-cfff-4163-ad96-784ae053a32e/dg07wy6-dbc8ced6-cddf-4408-98a2-6f2b98b0d261.png/v1/fill/w_1111,h_719/kamen_rider_tycoon_bujin_sword_form_render_by_neorider217_dg07wy6-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzM2IiwicGF0aCI6IlwvZlwvMzZlMWQ3ZTUtY2ZmZi00MTYzLWFkOTYtNzg0YWUwNTNhMzJlXC9kZzA3d3k2LWRiYzhjZWQ2LWNkZGYtNDQwOC05OGEyLTZmMmI5OGIwZDI2MS5wbmciLCJ3aWR0aCI6Ijw9MTEzNyJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.7aZg4W_RAJeJ6mBKdBjLKthvE5UE9ldU1x14AJ_9LkQ'); /* Set your image as a background */
+    /* background: url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/36e1d7e5-cfff-4163-ad96-784ae053a32e/dg07wy6-dbc8ced6-cddf-4408-98a2-6f2b98b0d261.png/v1/fill/w_1111,h_719/kamen_rider_tycoon_bujin_sword_form_render_by_neorider217_dg07wy6-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzM2IiwicGF0aCI6IlwvZlwvMzZlMWQ3ZTUtY2ZmZi00MTYzLWFkOTYtNzg0YWUwNTNhMzJlXC9kZzA3d3k2LWRiYzhjZWQ2LWNkZGYtNDQwOC05OGEyLTZmMmI5OGIwZDI2MS5wbmciLCJ3aWR0aCI6Ijw9MTEzNyJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.7aZg4W_RAJeJ6mBKdBjLKthvE5UE9ldU1x14AJ_9LkQ'); Set your image as a background */
     background-size: contain;
     background-repeat: no-repeat;
     padding: 2rem; /* Add padding to maintain space for content */
